8a1c2e18a7e3ca6d0f9146934b757102d335ff08
Tomat <xxlennygamerxx@gmail.com>
2023-08-15 18:17:07 -0700

    Then immediately scrap it because it isn't practical.

--- src/Tomat.LimbusServer.UrlRedirect/UrlRedirectMod.cs
+++ src/Tomat.LimbusServer.UrlRedirect/UrlRedirectMod.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Linq;
-using HarmonyLib;
+﻿using HarmonyLib;
 using Il2CppServer;
 using MelonLoader;
 using Tomat.LimbusServer.UrlRedirect;
-using UnityEngine;
 
 // ReSharper disable InconsistentNaming
 
@@ -34,47 +31,5 @@ public sealed class UrlRedirectMod : MelonMod {
         base.OnInitializeMelon();
 
         HarmonyInstance.PatchAll(typeof(UrlRedirectMod));
-        LogPackets();
-    }
-
-    private static void LogPackets() {
-        bool recursivelyCheckBaseType(Type type, Type baseType) {
-            if (type.BaseType is null)
-                return false;
-
-            if (!type.BaseType.IsGenericType)
-                return recursivelyCheckBaseType(type.BaseType, baseType);
-
-            if (type.BaseType.GetGenericTypeDefinition() == baseType)
-                return true;
-
-            return recursivelyCheckBaseType(type.BaseType, baseType);
-        }
-
-        var packets = typeof(HttpRequestCommand<,>).Assembly.GetTypes().Where(x => !x.IsAbstract && recursivelyCheckBaseType(x, typeof(HttpRequestCommand<,>))).ToList();
-        MelonLogger.Msg("Found " + packets.Count + " packets:");
-
-        foreach (var packet in packets) {
-            if ((packet.BaseType?.IsGenericType ?? false) && packet.BaseType == typeof(HttpRequestCommand<,>)) {
-                MelonLogger.Error("    " + packet.Name + " [unexpected base type: " + packet.BaseType + "]");
-            }
-            else {
-                MelonLogger.Msg("    " + packet.Name);
-                var requestType = packet.BaseType!.GetGenericArguments()[0];
-                var responseType = packet.BaseType!.GetGenericArguments()[1];
-                MelonLogger.Msg("        Request:  " + requestType?.Name);
-                MelonLogger.Msg("        Response: " + responseType?.Name);
-
-                foreach (var field in requestType!.GetFields()) {
-                    MelonLogger.Msg("            " + field.Name + " (" + field.FieldType.Name + ")");
-                    MelonLogger.Msg("                Has SerializeField: " + field.IsDefined(typeof(SerializeField), false));
-                }
-
-                foreach (var property in responseType!.GetProperties()) {
-                    MelonLogger.Msg("            " + property.Name + " (" + property.PropertyType.Name + ")");
-                    MelonLogger.Msg("                Has SerializeField: " + property.IsDefined(typeof(SerializeField), false));
-                }
-            }
-        }
     }
 }
